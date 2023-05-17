0eb0e9a2d33e8bd3f29c31d063c1f4fefdc48636
Yaqi Qin <yaqqin@student.ethz.ch>
2023-05-18 01:30:40 +0200

    train done

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# cs4nlp
+# Applying Mixture of Experts Technique on Small-Size Language Models for Multi-Step Mathematical Reasoning Problems
+
+## on euler server
+### conda
+```conda create -n csnlp_conda python=3.8```
+### Or venv
+```
+mkdir venvs
+python3 -m venv venvs/csnlp_venv
+source venvs/csnlp_venv/bin/activate
+```
+### euler module
+```module load gcc/8.2.0 python_gpu/3.10.4 boost/1.74.0 eth_proxy```
+
+```pip install -r requirements.txt```
+
+### login wandb
+```
+wandb login
+```
+
+### submit euler job
+```sbatch jobs/train```
--- data_prep.py
+++ data_prep.py
@@ -3,6 +3,7 @@ import json
 from collections import defaultdict
 import torch
 from torch.utils.data import Dataset
+import random
 
 def data_preprocess(file_path, output_path, sep = False):
 
@@ -74,17 +75,54 @@ def extract_subques_exp(dataset_path, output_path, num_steps=None):
             json.dump(examples[k], outfile)
         print("{} steps: {} data in total".format(k, len(examples[k])))
 
+def extract_all_exp(dataset_path, output_path):
+    """ Extract all num_steps of problems, and write into the json files
+    args:
+        dataset_path: the path to the dataset
+        output_path: the folder to write the processed data
+    """
+    
+    with open(dataset_path, 'r') as file:
+        all_data = json.load(file)
+
+    examples = []
+
+    for k, v in all_data.items():
+        concat_ans = ""
+        concat_questions = ""
+        for sub_q, sub_a in v[2]:
+            temp = {}
+            concat_questions += " " + sub_q
+            concat_ans += " " + sub_a 
+            temp["context"] = v[0] + " " + v[1] # context + main problem
+            temp["subquestions"] = concat_questions
+            temp["subanswers"] = concat_ans
+            examples.append(temp)
+
+    output_json_path = output_path + "/train_all_steps.json"
+    with open(output_json_path, "w") as outfile:
+        json.dump(examples, outfile)
+    print("{} data in total".format(len(examples)))
+
+def split_data(data, split_ratio=0.9):
+
+    # Split into training and validation sets    
+    train_size = int(split_ratio * len(data))
+
+    return data[:train_size], data[train_size:]
+
 class GSMDataset(Dataset):
     def __init__(self, tokenizer, examples, special_tokens, loss_on_prefix=True):
+        """Construct the input as <bos> context + main_q + all_subq <sep> all_suba <eos>
+        """
         self.examples = examples
-        self.context = [ex["context"] for ex in self.examples]
-        self.qns = [ex["question"] for ex in self.examples]
-        self.ans = [ex["answer"] for ex in self.examples]
+        self.context = [special_tokens["bos_token"] + ex["context"] for ex in self.examples]
+        self.qns = [ex["subquestions"] + special_tokens["sep_token"] for ex in self.examples]
+        self.ans = [ex["subanswers"] + special_tokens["eos_token"] for ex in self.examples]
         self.context = tokenizer(self.context, padding = False)
         self.qns = tokenizer(self.qns, padding=False)
         self.ans = tokenizer(self.ans, padding=False)
         self.loss_on_prefix = loss_on_prefix
-        self.special_tokens = special_tokens
         self.max_len = max(
             [
                 len(self.context["input_ids"][i]) + len(self.qns["input_ids"][i]) + len(self.ans["input_ids"][i])
@@ -97,9 +135,9 @@ class GSMDataset(Dataset):
         return len(self.examples)
 
     def __getitem__(self, idx):
-        context_tokens = self.special_tokens["bos_token"] + self.context["input_ids"][idx]
-        qn_tokens = self.qns["input_ids"][idx] + self.special_tokens["sep_token"]
-        ans_tokens = self.ans["input_ids"][idx] + self.special_tokens["eos_token"]
+        context_tokens = self.context["input_ids"][idx]
+        qn_tokens = self.qns["input_ids"][idx]
+        ans_tokens = self.ans["input_ids"][idx]
         pad_tokens = [0] * (self.max_len - len(context_tokens) - len(qn_tokens) - len(ans_tokens))
         tokens = context_tokens + qn_tokens + ans_tokens + pad_tokens
         mask = (
@@ -112,63 +150,9 @@ class GSMDataset(Dataset):
         mask = torch.tensor(mask)
         return dict(input_ids=tokens, attention_mask=mask)
 
-class myDataset(Dataset):
-
-    def __init__(self, data, tokenizer, randomize=True):
-
-        context, question, sub_q_a_pairs, answer= [], [], [], []
-        for k, v in data.items():
-            context.append(v[0])
-            question.append(v[1])
-            sub_q_a_pairs.append(v[2])
-            answer.append(v[3])
-
-        self.randomize = randomize
-        self.tokenizer = tokenizer
-        self.context = context
-        self.question = question
-        self.answer = answer
-        self.subq_a_pairs = sub_q_a_pairs
-
-
-    def __len__(self):
-        return len(self.answer)
-
-    def __getitem__(self, i):
-        input = SPECIAL_TOKENS['bos_token'] + \
-                SPECIAL_TOKENS['additional_special_tokens'][0] + self.context[i] + \
-                SPECIAL_TOKENS['additional_special_tokens'][1] + self.question[i]
-        
-        # append the previous answers
-        for j in range(len(self.subq_a_pairs[i])-1):
-            input += SPECIAL_TOKENS['additional_special_tokens'][2] + self.subq_a_pairs[i][j][1]
-        
-        # append the last sub question
-        input += SPECIAL_TOKENS['additional_special_tokens'][3] + self.subq_a_pairs[i][-1][0] + SPECIAL_TOKENS['eos_token']
-
-        # construct the label as the last sub answer
-        label = SPECIAL_TOKENS['bos_token'] + self.subq_a_pairs[i][-1][1] + SPECIAL_TOKENS['eos_token']
-
-        encodings_dict_input = self.tokenizer(input,
-                                   truncation=True,
-                                   max_length=MAXLEN,
-                                   padding="max_length")
-
-        encodings_dict_label = self.tokenizer(label,
-                                   truncation=True,
-                                   max_length=MAXLEN,
-                                   padding="max_length")
-        
-        input_ids = encodings_dict_input['input_ids']
-        attention_mask = encodings_dict_input['attention_mask']
-        label_ids = encodings_dict_label['input_ids']
-
-        return {'label': torch.tensor(label_ids),
-                'input_ids': torch.tensor(input_ids),
-                'attention_mask': torch.tensor(attention_mask)}
-
 
 if __name__ == '__main__':
     dataset_path = "dataset/train_socratic_processed_sep.json"
     output_path = "dataset"
-    extract_subques_exp(dataset_path, output_path, num_steps=None)
+    # extract_subques_exp(dataset_path, output_path, num_steps=None)
+    extract_all_exp(dataset_path, output_path)
