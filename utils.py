3211302ddee8bba441282d286892ecd0f875cf49
zhiyi chen <zhiyichen97@gmail.com>
2023-05-19 15:59:17 +0200

    Add inference time code

---
+++ utils.py
@@ -0,0 +1,24 @@
+import json
+from transformers import AutoTokenizer, AutoConfig, AutoModelForPreTraining, \
+                         AdamW, get_linear_schedule_with_warmup, \
+                         TrainingArguments, BeamScorer, Trainer
+
+import torch
+from torch.utils.data import Dataset, random_split, DataLoader, \
+                             RandomSampler, SequentialSampler
+MODEL = "gpt2"
+
+SPECIAL_TOKENS = {"bos_token": "<|BOS|>",
+                  "eos_token": "<|EOS|>",
+                  "unk_token": "<|UNK|>",
+                  "pad_token": "<|PAD|>",
+                  "sep_token": "<|SEP|>"}
+  
+def get_tokenier(special_tokens=None):
+    tokenizer = AutoTokenizer.from_pretrained(MODEL) #GPT2Tokenizer
+
+    if special_tokens:
+        tokenizer.add_special_tokens(special_tokens)
+        print("Special tokens added")
+    return tokenizer
+
